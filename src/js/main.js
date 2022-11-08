1febaf7d4bd9c9a4d3e5c830daccef1f244a87cd
Alukesh <akenzhebaev422@gmail.com>
2022-11-09 00:21:01 +0600

    my resume portfolio and contact links

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,24 @@
-const today = document.querySelector('#today')
+const wrapper = document.getElementById('bubble-wrapper')
+const animateBubble = x => {
+    const bubble = document.createElement("div")
+
+    bubble.className = "bubble"
+    bubble.style.left = `${x}px`
+
+    wrapper.appendChild(bubble)
+    setTimeout(() => wrapper.removeChild(bubble), 1500)
+}
+// window.onmousemove = e => {animateBubble(e.clientX)}
+
+const cards = document.querySelectorAll('.portfolio__card')
+// cards.forEach((card, i) => {
+//     card.addEventListener('mouseenter', () => card.classList.add('see'))
+//     card.addEventListener('mouseout', () => card.classList.remove('see'))
+// })
+const toDate = document.querySelector('#today')
+const today = new Date()
+const now = today.toLocaleString()
+toDate.textContent = now.slice(0,10)
+today.addEventListener('mouseout', () => alert(1))
+
 
