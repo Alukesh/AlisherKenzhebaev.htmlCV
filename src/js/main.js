a0777f460aaf8f5ddd662b12d97bd90ae75bb382
Alukesh <akenzhebaev422@gmail.com>
2023-06-24 23:16:04 +0600

    portfolio update dily, vue todo, posts saga, and more

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,8 @@
-const wrapper = document.getElementById('bubble-wrapper')
-const animateBubble = x => {
-    const bubble = document.createElement("div")
-
-    bubble.className = "bubble"
-    bubble.style.left = `${x}px`
-
-    wrapper.appendChild(bubble)
-    setTimeout(() => wrapper.removeChild(bubble), 1500)
-}
-// window.onmousemove = e => {animateBubble(e.clientX)}
-
 const cards = document.querySelectorAll('.portfolio__card')
-// cards.forEach((card, i) => {
-//     card.addEventListener('mouseenter', () => card.classList.add('see'))
-//     card.addEventListener('mouseout', () => card.classList.remove('see'))
-// })
+cards.forEach((card, i) => {
+    card.addEventListener('click', () => card.classList.toggle('see'))
+    // card.addEventListener('mouseout', () => card.classList.remove('see'))
+})
 const toDate = document.querySelector('#today')
 const today = new Date()
 const now = today.toLocaleString()
